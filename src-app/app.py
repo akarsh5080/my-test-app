32eb71faeb8394c21652c65d7e717271ff9f5be8
akarsh5080 <akarsh5080@gmail.com>
2024-02-03 17:11:02 +0530

    Updated the text in app.py

--- src-app/app.py
+++ src-app/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def welcome():
     user_agent_info = request.headers.get('User-Agent')
-    return f'Welcome to 2024!<br><br>User Agent: {user_agent_info}'
+    return f'Welcome to 2022!<br><br>User Agent: {user_agent_info}'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
