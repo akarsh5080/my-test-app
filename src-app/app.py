7e234b60e26de2cfcb9e4aa742df839773ab7716
akarsh5080 <akarsh5080@gmail.com>
2024-02-03 15:20:46 +0530

    first

---
+++ src-app/app.py
@@ -0,0 +1,13 @@
+#!/usr/local/bin/python3
+
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def welcome():
+    user_agent_info = request.headers.get('User-Agent')
+    return f'Welcome to 2024!<br><br>User Agent: {user_agent_info}'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
